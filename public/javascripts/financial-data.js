0cd00b94228f1c63951be0d8315584e65a01972e
Tomsylva <Tomsylvanz@gmail.com>
2021-03-27 17:55:27 +0100

    iteration 3 complete but maybe not working

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,19 +1,18 @@
 let axisX;
 let axisY;
+let apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
 
-axios
-  .get("http://api.coindesk.com/v1/bpi/historical/close.json")
-  .then((response) => {
-    // console.log(response.data.bpi);
-    axisX = Object.keys(response.data.bpi);
-    axisY = Object.values(response.data.bpi);
-    drawChart(axisX, axisY);
-  });
+axios.get(apiUrl).then((response) => {
+  console.log(response.data.bpi);
+  axisX = Object.keys(response.data.bpi);
+  axisY = Object.values(response.data.bpi);
+  drawChart(axisX, axisY);
+});
 
 function drawChart(labels, data) {
   const ctx = document.getElementById("myChart");
   const myChart = new Chart(ctx, {
-    type: "bar",
+    type: "line",
     data: {
       labels: labels,
       datasets: [
@@ -25,3 +24,30 @@ function drawChart(labels, data) {
     },
   });
 }
+
+let toValue;
+let fromValue;
+
+const changeDates = () => {
+  axios
+    .get(
+      `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromValue}&end=${toValue}`
+    )
+    .then((response) => {
+      axisX = Object.keys(response.data.bpi);
+      axisY = Object.values(response.data.bpi);
+      drawChart(axisX, axisY);
+    });
+};
+
+const dateFrom = document.getElementById("from");
+dateFrom.addEventListener("onclick", () => {
+  fromValue = document.getElementById("from").value;
+  changeDates();
+});
+
+const dateTo = document.getElementById("to");
+dateTo.addEventListener("onclick", () => {
+  toValue = document.getElementById("to").value;
+  changeDates();
+});
