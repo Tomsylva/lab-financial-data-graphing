f762ea189c91171ed7cf467489121dcf44481d40
Tomsylva <Tomsylvanz@gmail.com>
2021-03-27 16:49:12 +0100

    iteration 2 complete

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,28 +1,27 @@
+let axisX;
+let axisY;
+
 axios
   .get("http://api.coindesk.com/v1/bpi/historical/close.json")
   .then((response) => {
-    console.log(response.data.bpi);
-    const ctx = document.getElementById("myChart");
-    new Chart(ctx, {
-      type: "bar",
-      data: {
-        datasets: [
-          {
-            yAxisID: "",
-          },
-          {},
-        ],
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
-    });
+    // console.log(response.data.bpi);
+    axisX = Object.keys(response.data.bpi);
+    axisY = Object.values(response.data.bpi);
+    drawChart(axisX, axisY);
   });
+
+function drawChart(labels, data) {
+  const ctx = document.getElementById("myChart");
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Bitcoin Price Index",
+          data: data,
+        },
+      ],
+    },
+  });
+}
