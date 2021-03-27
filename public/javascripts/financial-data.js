eec8559c88815c4cd966f95d915418a1782957b2
Tomsylva <Tomsylvanz@gmail.com>
2021-03-27 16:24:50 +0100

    some of iteration 2 done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -2,4 +2,27 @@ axios
   .get("http://api.coindesk.com/v1/bpi/historical/close.json")
   .then((response) => {
     console.log(response.data.bpi);
+    const ctx = document.getElementById("myChart");
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        datasets: [
+          {
+            yAxisID: "",
+          },
+          {},
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
   });
