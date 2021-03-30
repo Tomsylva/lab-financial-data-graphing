7bf9723d29fb89ff3b56e57d1f83c993169fdcf6
Tomsylva <Tomsylvanz@gmail.com>
2021-03-30 19:28:44 +0200

    some changes made in class

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,9 +1,14 @@
 let axisX;
 let axisY;
 let apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+let start;
+let end;
+
+const startInput = document.getElementById("from");
+const endInput = document.getElementById("to");
 
 axios.get(apiUrl).then((response) => {
-  console.log(response.data.bpi);
+  console.log(response.data);
   axisX = Object.keys(response.data.bpi);
   axisY = Object.values(response.data.bpi);
   drawChart(axisX, axisY);
@@ -11,7 +16,7 @@ axios.get(apiUrl).then((response) => {
 
 function drawChart(labels, data) {
   const ctx = document.getElementById("myChart");
-  const myChart = new Chart(ctx, {
+  new Chart(ctx, {
     type: "line",
     data: {
       labels: labels,
@@ -25,29 +30,49 @@ function drawChart(labels, data) {
   });
 }
 
-let toValue;
-let fromValue;
+function getHistoricalDate() {
+  if (!end || !start) {
+    return;
+  }
+  axios.get(`${apiUrl}?start=${start}?end=${end}`).then((axiosResponse2) => {
+    const labels = Object.keys(axiosResponse2.data.bpi);
+    const data = Object.values(axiosResponse2.data.bpi);
+    drawChart(labels, data);
+  });
+}
 
-const changeDates = () => {
-  axios
-    .get(
-      `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromValue}&end=${toValue}`
-    )
-    .then((response) => {
-      axisX = Object.keys(response.data.bpi);
-      axisY = Object.values(response.data.bpi);
-      drawChart(axisX, axisY);
-    });
+startInput.onchange = (event) => {
+  console.log(event.target.value);
+  start = event.target.value;
+  getHistoricalDate();
 };
 
-const dateFrom = document.getElementById("from");
-dateFrom.addEventListener("onclick", () => {
-  fromValue = document.getElementById("from").value;
-  changeDates();
-});
+endInput.onchange = (event) => {
+  console.log(event.target.value);
+  end = event.target.value;
+  getHistoricalDate();
+};
 
-const dateTo = document.getElementById("to");
-dateTo.addEventListener("onclick", () => {
-  toValue = document.getElementById("to").value;
-  changeDates();
-});
+// const changeDates = () => {
+//   axios
+//     .get(
+//       `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromValue}&end=${toValue}`
+//     )
+//     .then((response) => {
+//       axisX = Object.keys(response.data.bpi);
+//       axisY = Object.values(response.data.bpi);
+//       drawChart(axisX, axisY);
+//     });
+// };
+
+// const dateFrom = document.getElementById("from");
+// dateFrom.addEventListener("onclick", () => {
+//   fromValue = document.getElementById("from").value;
+//   changeDates();
+// });
+
+// const dateTo = document.getElementById("to");
+// dateTo.addEventListener("onclick", () => {
+//   toValue = document.getElementById("to").value;
+//   changeDates();
+// });
